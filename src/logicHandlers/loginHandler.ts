dcb34053f7d1cf4e87aa41db2ecf369b305f248f
LeeSinHanma <kennydael213@gmail.com>
2026-02-01 17:31:06 +0800

    Adding Login logics

--- src/logicHandlers/loginHandler.ts
+++ src/logicHandlers/loginHandler.ts
@@ -1,1 +1,24 @@
-export {};
+const API_BASE = "https://flexolutions-backend-dev.onrender.com";
+
+export async function loginUser(
+  username: string,
+  password: string
+) {
+  const url = new URL(
+    `${API_BASE}/users/authenticate_user_endpoint_users_login_post`
+  );
+
+  url.searchParams.append("username", username);
+  url.searchParams.append("password", password);
+
+  const response = await fetch(url.toString(), {
+    method: "POST",
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    throw error;
+  }
+
+  return response.json();
+}
