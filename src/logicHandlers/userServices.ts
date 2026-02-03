99c3b07f651a43dc5ee285d3aa36c91071a4b161
Kenny Dael Asan <kennydael213@gmail.com>
2026-02-03 20:03:12 +0800

    Updated camera scanner and QR Module

--- src/logicHandlers/userServices.ts
+++ src/logicHandlers/userServices.ts
@@ -26,3 +26,15 @@ export async function loginUser(username: string, password: string): Promise<Log
     throw new Error(getErrorMessage(err));
   }
 }
+
+// ✅ POST /users/create ... creation of user(cashier/staffs)
+export async function createUser(username: string, email: string,password: string, firstName: string, lastName: string, role: number): Promise<LoginResponse> {
+  try {
+    const res = await api.post<LoginResponse>("/users/create", null, {
+      params: { username, email, password, firstName, lastName, role},
+    });
+    return res.data;
+  } catch (err) {
+    throw new Error(getErrorMessage(err));
+  }
+}
--- src/pages/EmployeePage/QRScanner.tsx
+++ src/pages/EmployeePage/QRScanner.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import QRScanner from "../../components/Reusable/QRScannerNav";
 import "./QRScanner.css";
 import { Button } from "../../components/Reusable/Button";
+import { useEffect } from "react";
+import { startQrScanner, stopQrScanner } from "../../logicHandlers/qrScannerModule";
 
 const QRScannerHome: React.FC = () => {
   const history = useHistory();
 
+  useEffect(() => {
+    startQrScanner(() => {});
+
+    return () => {
+      stopQrScanner();
+    };
+  }, []);
+
   return (
     <div className="main-pos-container">
       <div className="main-container">
-        <div className="qr-container"></div>
+        <div className="camera-container">
+          <div id="qr-reader" />
+        </div>
         <div className="menu-pos-container">
           <div className="add-member-container">
             <Button
@@ -28,6 +39,7 @@ const QRScannerHome: React.FC = () => {
             <div className="qr qr-container">
               <h3 onClick={() => history.push("/")}>QR Scanner</h3>
             </div>
+
             <div className="qr status-container">
               <h3 onClick={() => history.push("/status-member")}>Status</h3>
             </div>
