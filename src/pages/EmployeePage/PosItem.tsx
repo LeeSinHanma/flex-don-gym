f66ec9b1dfecbfc12e1e98050740681cb6cafcea
Kenny Dael Asan <kennydael213@gmail.com>
2026-03-14 17:53:45 +0800

    We can now search items in the POS

--- src/pages/EmployeePage/PosItem.tsx
+++ src/pages/EmployeePage/PosItem.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IonIcon } from "@ionic/react";
-import { searchOutline } from "ionicons/icons";
+import { searchOutline, arrowBackOutline } from "ionicons/icons";
 import "./PosItem.css";
 import { useHistory } from "react-router-dom";
 import POSCard from "../../components/Reusable/PosCard";
-import { cartOutline } from "ionicons/icons";
 import { BackButton } from "../../components/Reusable/BackButton";
-import { arrowBackOutline } from "ionicons/icons";
+import {
+  getInventoryItems,
+  InventoryItem,
+} from "../../logicHandlers/itemInvCrud";
 
 const PosItemPage: React.FC = () => {
   const history = useHistory();
+
+  const [items, setItems] = useState<InventoryItem[]>([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const loadItems = async () => {
+      try {
+        const data = await getInventoryItems();
+        setItems(data);
+      } catch (err) {
+        console.error("Failed to load inventory:", err);
+      }
+    };
+
+    loadItems();
+  }, []);
+
+  const filteredItems = items.filter((item) =>
+    item.item_name.toLowerCase().includes(searchText.toLowerCase()),
+  );
+
   return (
     <div className="pos-item-container">
       <div className="item-main-container">
@@ -24,24 +47,32 @@ const PosItemPage: React.FC = () => {
             </BackButton>
             <h2>All items</h2>
           </div>
+
           <div className="pos-search-bar">
             <div className="item-search-bar">
               <input
                 className="pos-search-input"
                 type="text"
-                placeholder="Search"
+                placeholder="Search item"
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
               />
             </div>
           </div>
         </div>
+
         <div className="pos-card-item">
-          <POSCard
-            productName="Protein Powder"
-            price={1000.0}
-            buttonLabel="Add to cart"
-            buttonIcon={<IonIcon icon={searchOutline} />}
-            onButtonClick={() => console.log("button clicked")}
-          />
+          {filteredItems.map((item) => (
+            <POSCard
+              key={item.item_id}
+              productName={item.item_name}
+              price={item.price}
+              stock={item.quantity} // stock
+              buttonLabel="Add to cart"
+              buttonIcon={<IonIcon icon={searchOutline} />}
+              onButtonClick={() => console.log("Add to cart:", item)}
+            />
+          ))}
         </div>
       </div>
     </div>
